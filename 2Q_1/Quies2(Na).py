edf0c8ba522a6982280f0fe6c77cacbe62761d95
filip157 <filipu157@gmail.com>
2023-01-19 09:38:10 +0800

    Create Quies2(Na).py

---
+++ 2Q_1/Quies2(Na).py
@@ -0,0 +1,18 @@
+card = []
+tmp = []
+time = 0
+
+for time in range(10):
+    card = card + [eval(input())]
+print(card)
+
+for time in range(10):
+    tmpnum = 0
+    num = []
+    if card[time] == card[time1]:
+        if num[time] == num[time1]:
+            tmpnum += 1
+        elif num[]
+    print(num[0])
+    print(len(num))
+
