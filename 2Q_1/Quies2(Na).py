a6c989aaaa22d03edaf4503a2e9f5643901ef2b0
filip157 <filipu157@gmail.com>
2023-02-02 11:31:26 +0800

    Update Quies2(Na).py

--- 2Q_1/Quies2(Na).py
+++ 2Q_1/Quies2(Na).py
@@ -1,18 +1,22 @@
 card = []
-tmp = []
-time = 0
+times = []
+position = 0
 
 for time in range(10):
     card = card + [eval(input())]
+    if (card[time] not in times):
+        times[time] = 1
+    elif (card[time] in times):
+
+    else:
+        eos = 0
 print(card)
 
 for time in range(10):
     tmpnum = 0
     num = []
-    if card[time] == card[time1]:
-        if num[time] == num[time1]:
-            tmpnum += 1
-        elif num[]
+    for time1 in range(10):
+        if card[time] == card[time1]:
+                tmpnum += 1
     print(num[0])
     print(len(num))
-
